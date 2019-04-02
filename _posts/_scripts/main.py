5628609e0f6c5aa98556e8ef0d6a788f236cfe34
Maksim <maksim.levental@gmail.com>
2019-04-02 00:31:17 -0400

    dsp

--- _posts/_scripts/main.py
+++ _posts/_scripts/main.py
@@ -1,15 +1,27 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-A = 5
+# A = 5
+# plt.rc('text', usetex=True)
+# plt.rc('font', family='serif')
+# plt.stem(
+#     [0]+[A/(n*np.pi) for n in range(1,20)],
+# )
+# plt.xlabel("$n$", fontsize=18)
+# plt.ylabel(r"$b_n$", fontsize=24)
+# plt.savefig("/home/maksim/dev_projects/makslevental.github.io/images/sawtooth_freq_plot.png")
+
 plt.rc('text', usetex=True)
 plt.rc('font', family='serif')
-plt.stem(
-    [0]+[A/(n*np.pi) for n in range(1,20)],
-)
+A = 2/np.pi
+plt.stem([0,1,2,3,4,5], [0,A,-A/2,A/3,-A/4,A/5])
+plt.step([0,1,2,3,4], [0,1,1,0,0], where='post', color='red')
+plt.fill_between([1,2,3], 0, 1, facecolor='red', alpha=0.3)
+
 plt.xlabel("$n$", fontsize=18)
 plt.ylabel(r"$b_n$", fontsize=24)
-plt.savefig("/home/maksim/dev_projects/makslevental.github.io/images/sawtooth_freq_plot.png")
+# plt.show()
+plt.savefig("/home/maksim/dev_projects/makslevental.github.io/images/sawtooth_filter_freq_plot.png")
 # # Example data
 # t = np.arange(0.0, 1.0 + 0.01, 0.01)
 # s = np.cos(4 * np.pi * t) + 2
